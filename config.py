e0109837c275277439f338d92194dc21731d3793
ani-per_github <amp7226@psu.edu>
2024-03-23 14:45:43 -0400

    split parameters into `config.py`, add docstrings

---
+++ config.py
@@ -0,0 +1,16 @@
+from storyboarder import *
+
+set_name = f"Untitled Film Set" # Name of the set/packet
+db_dir = Path.cwd() / "demo" # Directory where the answerline document should be saved
+db_path = (db_dir / f"{set_name.title().replace(' ', '-')}_Database.csv") # Filepath of the answerline database CSV
+
+storyboard(
+    set_name,
+    db_dir,
+    db_path,
+    hybrid=True, # Is the set a hybrid visual-written tournament?
+    raw_string="W", # The suffix to identify the written packets
+    set_dir=Path.home() / "Documents" / "quizbowl" / "oligo" / "tournaments" / "untitled-film-set" / "packets", # The directory where the written packets are stored (and where the hybrid packets will be generated)
+    split_docs=False, # Should the answerline documents be split?
+    verbose=True # Print progress
+)
--- storyboarder.py
+++ storyboarder.py
@@ -1,4 +1,3 @@
-import itertools as it  # Readable nested for loops
 from pathlib import Path  # Filepaths
 import typing # Argument / output type checking
 import pandas as pd # DataFrames
@@ -19,10 +18,21 @@ def _copy(self, target):
 Path.copy = _copy
 
 # https://stackoverflow.com/a/38234962
-def make_curly(str):
+def make_curly(str: str) -> str:
     return sub(r"(\s|^)\'(.*?)\'(\s|$)", r"\1‘\2’\3", sub(r"\"(.*?)\"", r"“\1”", str)).replace("\'", "’")
 
-def write_answerline(ans_par, ans_raw, ans_type):
+def style_doc(tmpl):
+    # tmpl.styles["Heading 1"].font = ""
+    pass
+
+def write_answerline(ans_par: docx.text.paragraph.Paragraph, ans_raw: str, ans_type: str):
+    """Write the database answerline to a given paragraph in a document.
+
+    Args:
+        ans_par (docx.text.paragraph.Paragraph): The paragraph to which the answerline should be written.
+        ans_raw (str): The raw (unformatted) answerline from the answerline database.
+        ans_type (str): The class of the answerline in the database (e.g. "Film", "Director", "Crew", "Figure", "Misc").
+    """
     ans_split = ans_raw.split(" [")
     main_ans_split = ans_split[0].split(" (")
     main_ans = main_ans_split[0]
@@ -90,187 +100,194 @@ def write_answerline(ans_par, ans_raw, ans_type):
                     else:
                         ans_par.add_run("; ")
 
-def style_doc(tmpl):
-    # tmpl.styles["Heading 1"].font = ""
-    pass
+def storyboard(
+    set_name: str,
+    db_dir: Path,
+    db_path: Path,
+    hybrid: bool = True,
+    raw_string: str = "_raw",
+    set_dir: Path = None,
+    split_docs: bool = False,
+    verbose: bool = False
+):
+    """Create the visual answerline document, and hybrid packets along the way if desired.
 
-# Set parameters
+    Args:
+        set_name (str): Name of the set/packet
+        db_dir (Path): Directory where the answerline database CSV is stored (and where the visual answerline document will be generated)
+        db_path (Path): Filepath of the answerline database CSV
+        hybrid (bool, optional): Is the set a hybrid visual-written tournament? Defaults to True.
+        raw_string (str, optional): Suffix to identify the written packets. Defaults to "_raw".
+        set_dir (Path, optional): Directory where the written packets are stored (and where the hybrid packets will be generated). Defaults to None.
+        split_docs (bool, optional): Should the answerline documents be split? Defaults to False.
+        verbose (bool, optional): Print progress. Defaults to False.
+    """
 
-verbose = True
+    ans_db = pd.read_csv(db_path).convert_dtypes()
 
-set_name = f"Untitled Film Set"
-set_slug = set_name.title().replace(" ", "-")
+    if set_dir == None:
+        set_dir = db_dir
 
-db_dir = Path.cwd() / "demo"
-ans_db = pd.read_csv((db_dir / f"{set_slug}_Database.csv")).convert_dtypes() # Source: Database CSV
+    set_slug = set_name.title().replace(" ", "-")
 
-# Is the set a hybrid visual-written tournament?
-hybrid = True
-raw_string = "W" # "_raw"
-set_dir = Path.home() / "Documents" / "quizbowl" / "oligo" / "tournaments" / "untitled-film-set" / "packets"
+    packet_names = list(ans_db["Packet"].unique())
+    n_packet = len(packet_names)
 
-split_docs = False # Should the answerline documents be split?
+    templates = n_packet*[None]
+    documents = n_packet*[None]
+    packets = n_packet*[None]
+    answers = n_packet*[ans_db["Number"].max()*[None]]
+    slides = n_packet*[ans_db["Number"].max()*[[None]]]
 
-# Begin processing
+    for i in range(n_packet):
+        if split_docs or (not split_docs and (i == 0)):
+            templates[i] = docx.Document()
+            if split_docs:
+                documents[i] = (db_dir / f"{set_slug}_Answers-raw_{packet_names[i].zfill(2)}.docx")
+            elif not split_docs and (i == 0):
+                documents[i] = (db_dir / f"{set_slug}_Answers-raw.docx")
+        elif (not split_docs) and (i > 0):
+            templates[i] = templates[0]
+            documents[i] = documents[0]
 
-packet_names = list(ans_db["Packet"].unique())
-n_packet = len(packet_names)
+    # Use curly quotes
+    for col in ["Answerline", "Source", "Director", "Notes"]:
+        ans_db[col] = ans_db[col].apply(lambda s: make_curly(s) if pd.notnull(s) else s)
 
-templates = n_packet*[None]
-documents = n_packet*[None]
-packets = n_packet*[None]
-answers = n_packet*[ans_db["Number"].max()*[None]]
-slides = n_packet*[ans_db["Number"].max()*[[None]]]
+    # Prepare the hybrid packet generation, if configured
+    make_hybrid = True
+    if hybrid and set_dir.exists():
+        written_packets = sorted(set_dir.glob(f"**/*{raw_string}.docx"))
+        if len(list(written_packets)) == n_packet:
+            make_hybrid = True
+            hybrid_packets = n_packet*[None]
+            hybrid_answers = n_packet*[ans_db["Number"].max()*[None]]
+        else:
+            print("Number of placeholder packets doesn't match the number of written packets.")
 
-for i in range(n_packet):
-    if split_docs or (not split_docs and (i == 0)):
-        templates[i] = docx.Document()
-        if split_docs:
-            documents[i] = (db_dir / f"{set_slug}_Answers-raw_{packet_names[i].zfill(2)}.docx")
-        elif not split_docs and (i == 0):
-            documents[i] = (db_dir / f"{set_slug}_Answers-raw.docx")
-    elif (not split_docs) and (i > 0):
-        templates[i] = templates[0]
-        documents[i] = documents[0]
+    for i in range(n_packet): # Loop over packets
+        style_doc(templates[i])
+        if (split_docs) or ((not split_docs) and (i == 0)):
+            templates[i].add_heading(f"{set_name} - Visual Answerlines", level=0)
+        elif (not split_docs) and (i > 0):
+            templates[i].add_page_break()
+        packet_header = f"Packet {packet_names[i]}"
+        packets[i] = templates[i].add_heading(packet_header, level=1)
 
-# Use curly quotes
-for col in ["Answerline", "Source", "Director", "Notes"]:
-    ans_db[col] = ans_db[col].apply(lambda s: make_curly(s) if pd.notnull(s) else s)
+        if verbose:
+            print(packet_header)
 
-# Prepare the hybrid packet generation, if configured
-make_hybrid = True
-if hybrid and set_dir.exists():
-    written_packets = sorted(set_dir.glob(f"**/*{raw_string}.docx"))
-    if len(list(written_packets)) == n_packet:
-        make_hybrid = True
-        hybrid_packets = n_packet*[None]
-        hybrid_answers = n_packet*[ans_db["Number"].max()*[None]]
-    else:
-        print("Number of placeholder packets doesn't match the number of written packets.")
+        # Filter the database to the current packet
+        packet_db = ans_db[ans_db["Packet"] == packet_names[i]]
 
-for i in range(n_packet): # Loop over packets
-    style_doc(templates[i])
-    if (split_docs) or ((not split_docs) and (i == 0)):
-        templates[i].add_heading(f"{set_name} - Visual Answerlines", level=0)
-    elif (not split_docs) and (i > 0):
-        templates[i].add_page_break()
-    packet_header = f"Packet {packet_names[i]}"
-    packets[i] = templates[i].add_heading(packet_header, level=1)
+        # If hybrid, make the current hybrid packet by appending to the corresponding written packet
+        if make_hybrid:
+            hybrid_packets[i] = written_packets[i].parent / f"{set_slug}_{packet_names[i].zfill(2)}.docx"
+            if hybrid_packets[i].exists():
+                hybrid_packets[i].unlink()
+            Path.copy(written_packets[i], hybrid_packets[i])
+            hybrid_docx = docx.Document(hybrid_packets[i])
+            # Calculate the number of already-written questions in the packet
+            # This compiles all the numbers in the document that are succeeded by a period, then takes the maximum
+            # https://stackoverflow.com/questions/952914/how-do-i-make-a-flat-list-out-of-a-list-of-lists#comment123215183_952952
+            n_written = max([int(s) for s in [leaf for tree in [findall("(^\d+)\.+", s) for s in [p.text for p in hybrid_docx.paragraphs]] for leaf in tree if leaf]])
 
-    if verbose:
-        print(packet_header)
+        for j in range(packet_db["Number"].max()): # Loop over questions
+            # Filter the database to the current question
+            q_db = packet_db[packet_db["Number"] == (j + 1)]
+            n_slide = q_db.shape[0]
 
-    # Filter the database to the current packet
-    packet_db = ans_db[ans_db["Packet"] == packet_names[i]]
+            # Only process if the answerline is not empty
+            if pd.notnull(q_db.iloc[0]["Answerline"]):
+                # Write the answerline
+                answers[i][j] = templates[i].add_paragraph("", style="List Number")
+                write_answerline(answers[i][j], q_db.iloc[0]["Answerline"], q_db.iloc[0]["Type"])
 
-    # If hybrid, make the current hybrid packet by appending to the corresponding written packet
-    if make_hybrid:
-        hybrid_packets[i] = written_packets[i].parent / f"{set_slug}_{packet_names[i].zfill(2)}.docx"
-        if hybrid_packets[i].exists():
-            hybrid_packets[i].unlink()
-        Path.copy(written_packets[i], hybrid_packets[i])
-        hybrid_docx = docx.Document(hybrid_packets[i])
-        # Calculate the number of already-written questions in the packet
-        # This compiles all the numbers in the document that are succeeded by a period, then takes the maximum
-        # https://stackoverflow.com/questions/952914/how-do-i-make-a-flat-list-out-of-a-list-of-lists#comment123215183_952952
-        n_written = max([int(s) for s in [leaf for tree in [findall("(^\d+)\.+", s) for s in [p.text for p in hybrid_docx.paragraphs]] for leaf in tree if leaf]])
+                if verbose:
+                    print(f"{j + 1}: " + q_db.iloc[0]["Answerline"])
 
-    for j in range(packet_db["Number"].max()): # Loop over questions
-        # Filter the database to the current question
-        q_db = packet_db[packet_db["Number"] == (j + 1)]
-        n_slide = q_db.shape[0]
+                # If hybrid, make the placeholder question
+                if make_hybrid:
+                    hybrid_docx.add_paragraph("")
+                    slide_q = hybrid_docx.add_paragraph(f"{j + n_written + 1}. ")
+                    slide_runs = n_slide*[None]
+                    for k in range(n_slide): # Loop over slides
+                        slide_runs[k] = slide_q.add_run(f"{k + 1}" + f" "*(k < (n_slide - 1)))
+                        if k < (n_slide - 1):
+                            if q_db.iloc[k]["Value"] == 20:
+                                slide_runs[k].bold = True
+                                slide_runs[k].underline = True
+                                if q_db.iloc[k + 1]["Value"] < 20:
+                                    superpower_mark = slide_q.add_run(f"(+)")
+                                    superpower_mark.bold = True
+                                    superpower_mark.underline = True
+                                    slide_q.add_run(f" ")
+                            elif q_db.iloc[k]["Value"] == 15:
+                                slide_runs[k].bold = True
+                                if q_db.iloc[k + 1]["Value"] < 15:
+                                    power_mark = slide_q.add_run(f"(*)")
+                                    power_mark.bold = True
+                                    slide_q.add_run(f" ")
 
-        # Only process if the answerline is not empty
-        if pd.notnull(q_db.iloc[0]["Answerline"]):
-            # Write the answerline
-            answers[i][j] = templates[i].add_paragraph("", style="List Number")
-            write_answerline(answers[i][j], q_db.iloc[0]["Answerline"], q_db.iloc[0]["Type"])
+                    hybrid_answers[i][j] = hybrid_docx.add_paragraph("ANSWER: ")
+                    write_answerline(hybrid_answers[i][j], q_db.iloc[0]["Answerline"], q_db.iloc[0]["Type"])
 
-            if verbose:
-                print(f"{j + 1}: " + q_db.iloc[0]["Answerline"])
-
-            # If hybrid, make the placeholder question
-            if make_hybrid:
-                hybrid_docx.add_paragraph("")
-                slide_q = hybrid_docx.add_paragraph(f"{j + n_written + 1}. ")
-                slide_runs = n_slide*[None]
-                for k in range(n_slide): # Loop over slides
-                    slide_runs[k] = slide_q.add_run(f"{k + 1}" + f" "*(k < (n_slide - 1)))
-                    if k < (n_slide - 1):
-                        if q_db.iloc[k]["Value"] == 20:
-                            slide_runs[k].bold = True
-                            slide_runs[k].underline = True
-                            if q_db.iloc[k + 1]["Value"] < 20:
-                                superpower_mark = slide_q.add_run(f"(+)")
-                                superpower_mark.bold = True
-                                superpower_mark.underline = True
-                                slide_q.add_run(f" ")
-                        elif q_db.iloc[k]["Value"] == 15:
-                            slide_runs[k].bold = True
-                            if q_db.iloc[k + 1]["Value"] < 15:
-                                power_mark = slide_q.add_run(f"(*)")
-                                power_mark.bold = True
-                                slide_q.add_run(f" ")
-
-                hybrid_answers[i][j] = hybrid_docx.add_paragraph("ANSWER: ")
-                write_answerline(hybrid_answers[i][j], q_db.iloc[0]["Answerline"], q_db.iloc[0]["Type"])
-
-            if q_db.iloc[0]["Type"] == "Film": # If it's a film, write the director in the answerline
-                dir_raw = f" (dir. {q_db.iloc[0]['Director']})"
-                answers[i][j].add_run(dir_raw)
-                if (make_hybrid):
-                    hybrid_answers[i][j].add_run(dir_raw)
-            else: # Prepare the slide annotations, if it's not a film
-                slides[i][j] = n_slide*[None]
-                for k in range(n_slide): # Loop over slides
-                    # Add an annotation if there's a source listed for the current slide
-                    if pd.isna(q_db.iloc[k]["Source"]):
-                        src_raw = ""
-                    else:
-                        src_raw = q_db.iloc[k]["Source"]
-
-                    # Write the slide annotation
-                    slides[i][j][k] = templates[i].add_paragraph("", style="List Number 2")
-                    src_run = slides[i][j][k].add_run(src_raw)
-                    if (len(src_raw) > 0) and not (q_db.iloc[k]["Source"].startswith(("\'", "\"", "‘", "“"))): # Don't italicize if title's in quotes (e.g. music video)
-                        src_run.italic = True
-
-                    # If the question's not a Director, add the director credit for the source of the current slide
-                    if (q_db.iloc[0]["Type"] != "Director") or (q_db.iloc[0]["Type"] == "Director" and not pd.isna(q_db.iloc[k]["Director"])):
-                        if (k > 0) and (pd.isna(q_db.iloc[k]["Director"])) and not (pd.isna(q_db.iloc[0]["Director"])):
-                            dir_raw = f" (dir. {q_db.iloc[0]['Director']})"
-                        elif not (pd.isna(q_db.iloc[k]["Director"])):
-                            dir_raw = f" (dir. {q_db.iloc[k]['Director']})"
+                if q_db.iloc[0]["Type"] == "Film": # If it's a film, write the director in the answerline
+                    dir_raw = f" (dir. {q_db.iloc[0]['Director']})"
+                    answers[i][j].add_run(dir_raw)
+                    if (make_hybrid):
+                        hybrid_answers[i][j].add_run(dir_raw)
+                else: # Prepare the slide annotations, if it's not a film
+                    slides[i][j] = n_slide*[None]
+                    for k in range(n_slide): # Loop over slides
+                        # Add an annotation if there's a source listed for the current slide
+                        if pd.isna(q_db.iloc[k]["Source"]):
+                            src_raw = ""
                         else:
-                            dir_raw = ""
-                        slides[i][j][k].add_run(dir_raw)
+                            src_raw = q_db.iloc[k]["Source"]
 
-                    # Format the annotation as a list element
-                    if k == 0:
-                        list_number(templates[i], slides[i][j][k], prev=None, level=0)
-                    else:
-                        list_number(templates[i], slides[i][j][k], prev=slides[i][j][k - 1])
+                        # Write the slide annotation
+                        slides[i][j][k] = templates[i].add_paragraph("", style="List Number 2")
+                        src_run = slides[i][j][k].add_run(src_raw)
+                        if (len(src_raw) > 0) and not (q_db.iloc[k]["Source"].startswith(("\'", "\"", "‘", "“"))): # Don't italicize if title's in quotes (e.g. music video)
+                            src_run.italic = True
 
-            # If there are notes, write them
-            if not pd.isna(q_db.iloc[0]["Notes"]):
-                notes_raw = f" ({q_db.iloc[0]['Notes']})"
-                answers[i][j].add_run(notes_raw)
+                        # If the question's not a Director, add the director credit for the source of the current slide
+                        if (q_db.iloc[0]["Type"] != "Director") or (q_db.iloc[0]["Type"] == "Director" and not pd.isna(q_db.iloc[k]["Director"])):
+                            if (k > 0) and (pd.isna(q_db.iloc[k]["Director"])) and not (pd.isna(q_db.iloc[0]["Director"])):
+                                dir_raw = f" (dir. {q_db.iloc[0]['Director']})"
+                            elif not (pd.isna(q_db.iloc[k]["Director"])):
+                                dir_raw = f" (dir. {q_db.iloc[k]['Director']})"
+                            else:
+                                dir_raw = ""
+                            slides[i][j][k].add_run(dir_raw)
 
-            if j == 0:
-                list_number(templates[i], answers[i][j], prev=None, level=0)
-            else:
-                list_number(templates[i], answers[i][j], prev=answers[i][j - 1])
+                        # Format the annotation as a list element
+                        if k == 0:
+                            list_number(templates[i], slides[i][j][k], prev=None, level=0)
+                        else:
+                            list_number(templates[i], slides[i][j][k], prev=slides[i][j][k - 1])
 
-    if make_hybrid:
-        hybrid_docx.save(hybrid_packets[i])
-    # Write the document
-    templates[i].save(documents[i])
+                # If there are notes, write them
+                if not pd.isna(q_db.iloc[0]["Notes"]):
+                    notes_raw = f" ({q_db.iloc[0]['Notes']})"
+                    answers[i][j].add_run(notes_raw)
 
-if not split_docs:
-    # https://stackoverflow.com/a/435669
-    if system() == "Darwin": # MacOS
-        call(("open", documents[0]))
-    elif system() == "Windows": # Windows
-        startfile(documents[0])
-    else: # Linux variants
-        call(("xdg-open", documents[0]))
+                if j == 0:
+                    list_number(templates[i], answers[i][j], prev=None, level=0)
+                else:
+                    list_number(templates[i], answers[i][j], prev=answers[i][j - 1])
+
+        if make_hybrid:
+            hybrid_docx.save(hybrid_packets[i])
+        # Write the document
+        templates[i].save(documents[i])
+
+    if not split_docs:
+        # https://stackoverflow.com/a/435669
+        if system() == "Darwin": # MacOS
+            call(("open", documents[0]))
+        elif system() == "Windows": # Windows
+            startfile(documents[0])
+        else: # Linux variants
+            call(("xdg-open", documents[0]))
